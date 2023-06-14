bd26accff3cfb5fe4c8cdb960c2840223405926f
Gonzalo Martínez <ggmartinez.utu@gmail.com>
2023-06-13 22:48:44 -0300

    Forzar JSON por defecto Agregamos modelo de ejemplo Creamos controller vacio

--- API/App_Start/WebApiConfig.cs
+++ API/App_Start/WebApiConfig.cs
@@ -19,6 +19,9 @@ namespace API
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Forzar JSON por defecto
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
         }
     }
 }
---
+++ API/Models/VideojuegoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class VideojuegoModel
+    {
+        public int Id;
+        public string Nombre;
+        public string FechaDeLanzamiento;
+        public string Genero;
+    }
+}
